18173a601793f61e66d9e8e675430ae9657ce536
hoapng <giahoa01giathieu@gmail.com>
2024-04-07 10:37:46 +0700

    test fetch data

--- src/app/profile/[slug]/page.tsx
+++ src/app/profile/[slug]/page.tsx
@@ -1,8 +1,14 @@
 import { Avatar } from "antd";
 import { AntDesignOutlined } from "@ant-design/icons";
 import Post from "@/components/post/Post";
+import { sendRequest } from "@/utils/api";
 
-const Profile: React.FC = () => {
+const Profile: React.FC = async () => {
+  const user = await sendRequest({
+    url: "http://localhost:8000/api/v1/users/66010ee0ad519e20b635c6e9",
+    method: "GET",
+  });
+  console.log(user);
   return (
     <>
       <Avatar
