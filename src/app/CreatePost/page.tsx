80123aa8ee41239736e68e4de45381b93ab9fd94
hoapng <giahoa01giathieu@gmail.com>
2024-04-24 21:51:19 +0700

    creat post page

--- src/app/CreatePost/page.tsx
+++ src/app/CreatePost/page.tsx
@@ -1,54 +1,147 @@
 "use client";
-// import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import { useState } from "react";
 import Editor from "@/components/Editor";
-import { useRouter } from "next/navigation";
+import React, { useCallback, useRef, useState } from "react";
+import { Mentions } from "antd";
+import debounce from "lodash/debounce";
+import { PlusOutlined } from "@ant-design/icons";
+import { Button, DatePicker, Form, Input, Select, Upload } from "antd";
+import { sendRequest } from "@/utils/api";
+import { error } from "console";
+
+const { TextArea } = Input;
+
+export enum TweetType {
+  Post,
+  Share,
+  Comment,
+  News,
+  Review,
+  Tips,
+  Question,
+}
+
+const normFile = (e: any) => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
+};
 
 export default function CreatePost() {
-  const router = useRouter();
-  const [title, setTitle] = useState("");
-  const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
-  const [files, setFiles] = useState("");
-  const [redirect, setRedirect] = useState(false);
-  async function createNewPost(ev) {
-    const data = new FormData();
-    data.set("title", title);
-    data.set("summary", summary);
-    data.set("content", content);
-    data.set("file", files[0]);
-    ev.preventDefault();
-    const response = await fetch("http://localhost:4000/post", {
-      method: "POST",
-      body: data,
-      credentials: "include",
-    });
-    if (response.ok) {
-      setRedirect(true);
-    }
-  }
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const ref = useRef<string>();
 
-  if (redirect) {
-    return router.push("/");
-  }
+  const loadGithubUsers = async (key: string) => {
+    if (!key) {
+      setUsers([]);
+      return;
+    }
+
+    let date = new Date();
+
+    const res = await sendRequest({
+      url: `http://localhost:8000/api/v1/hashtag/top`,
+      method: "GET",
+      queryParams: {
+        current: 1,
+        pageSize: 5,
+        from: date.setDate(date.getDate() - 7),
+        name: `/${key}/`,
+      },
+      nextOption: {
+        cache: "no-store",
+      },
+    });
+    if (res && res.data) {
+      if (ref.current !== key) return;
+      setLoading(false);
+      setUsers(res.data.result);
+    }
+
+    // fetch(`https://api.github.com/search/users?q=${key}`)
+    //   .then((res) => res.json())
+    //   .then(({ items = [] }) => {
+    //     if (ref.current !== key) return;
+
+    //     setLoading(false);
+    //     setUsers(items.slice(0, 10));
+    //     console.log(users);
+    //   });
+  };
+
+  const debounceLoadGithubUsers = useCallback(
+    debounce(loadGithubUsers, 500),
+    []
+  );
+
+  const onSearch = (search: string) => {
+    console.log("Search:", search);
+    ref.current = search;
+    setLoading(!!search);
+    setUsers([]);
+
+    debounceLoadGithubUsers(search);
+  };
+
   return (
-    <form onSubmit={createNewPost}>
-      <input
-        type="title"
-        placeholder={"Title"}
-        value={title}
-        onChange={(ev) => setTitle(ev.target.value)}
-      />
-      <input
-        type="summary"
-        placeholder={"Summary"}
-        value={summary}
-        onChange={(ev) => setSummary(ev.target.value)}
-      />
-      <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
-      <Editor value={content} onChange={setContent} />
-      <button style={{ marginTop: "5px" }}>Create post</button>
-    </form>
+    <>
+      <Form layout="vertical">
+        <Form.Item label="Select">
+          <Select>
+            <Select.Option value={TweetType.News}>News</Select.Option>
+            <Select.Option value={TweetType.Post}>Post</Select.Option>
+            <Select.Option value={TweetType.Question}>Question</Select.Option>
+            <Select.Option value={TweetType.Review}>Review</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item label="Title">
+          <TextArea rows={1} />
+        </Form.Item>
+
+        <Form.Item label="Trending">
+          <Mentions
+            prefix="#"
+            style={{ width: "100%" }}
+            loading={loading}
+            onSearch={onSearch}
+            options={users.map(({ _id }) => ({
+              key: _id,
+              value: _id,
+              className: "antd-demo-dynamic-option",
+              label: (
+                <>
+                  <span>{_id}</span>
+                </>
+              ),
+            }))}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Upload"
+          valuePropName="fileList"
+          getValueFromEvent={normFile}
+        >
+          <Upload action="/upload.do" listType="picture-card">
+            <button style={{ border: 0, background: "none" }} type="button">
+              <PlusOutlined />
+              <div style={{ marginTop: 8 }}>Upload</div>
+            </button>
+          </Upload>
+        </Form.Item>
+
+        <Form.Item>
+          <Editor value={content} onChange={setContent} />
+        </Form.Item>
+
+        <Form.Item>
+          <Button>Submit</Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 }
