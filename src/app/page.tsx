bf0e570afdc140f39cd08cdb376d7e2b2e694b3c
hoapng <giahoa01giathieu@gmail.com>
2024-07-04 18:26:47 +0700

    fix6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -120,131 +120,127 @@ const App: React.FC = async () => {
 
   // const tips = await getTips();
   return (
-    <Suspense
-      fallback={<Spinner aria-label="Extra large spinner example" size="xl" />}
-    >
-      <div className="p-3 max-w-7xl mx-auto min-h-screen">
-        <div className="flex flex-col">
-          <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-            <Carousel className="w-full">
+    <div className="p-3 max-w-7xl mx-auto min-h-screen">
+      <div className="flex flex-col">
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+          <Carousel className="w-full">
+            {news.result.map((x: any, index: number) => {
+              return (
+                <div key={index}>
+                  {x.files.length > 0 ? (
+                    <Link href={`/Post/${x._id}`} className="">
+                      <img
+                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/uploadedFiles/${x.files}`}
+                        alt={x.title}
+                      />
+                    </Link>
+                  ) : (
+                    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-56 sm:h-64 xl:h-80 2xl:h-96 place-content-center">
+                      <p className="text-white text-4xl w-8/12 mx-auto text-center">
+                        {x.title}
+                      </p>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+            {review.result.map((x: any, index: number) => {
+              return (
+                <div key={index}>
+                  {x.files.length > 0 ? (
+                    <Link href={`/Post/${x._id}`} className="">
+                      <img
+                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/uploadedFiles/${x.files}`}
+                        alt={x.title}
+                      />
+                    </Link>
+                  ) : (
+                    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-56 sm:h-64 xl:h-80 2xl:h-96 place-content-center">
+                      <p className="text-white text-4xl w-8/12 mx-auto text-center">
+                        {x.title}
+                      </p>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+            {tips.result.map((x: any, index: number) => {
+              return (
+                <div key={index}>
+                  {x.files.length > 0 ? (
+                    <Link href={`/Post/${x._id}`} className="">
+                      <img
+                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/uploadedFiles/${x.files}`}
+                        alt={x.title}
+                      />
+                    </Link>
+                  ) : (
+                    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-56 sm:h-64 xl:h-80 2xl:h-96 place-content-center">
+                      <p className="text-white text-4xl w-8/12 mx-auto text-center">
+                        {x.title}
+                      </p>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </Carousel>
+        </div>
+        <br />
+        <div className="flex w-full">
+          <div className="w-4/12">
+            <Timeline>
               {news.result.map((x: any, index: number) => {
                 return (
-                  <div key={index}>
-                    {x.files.length > 0 ? (
-                      <Link href={`/Post/${x._id}`} className="">
-                        <img
-                          src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/uploadedFiles/${x.files}`}
-                          alt={x.title}
-                        />
-                      </Link>
-                    ) : (
-                      <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-56 sm:h-64 xl:h-80 2xl:h-96 place-content-center">
-                        <p className="text-white text-4xl w-8/12 mx-auto text-center">
-                          {x.title}
-                        </p>
-                      </div>
-                    )}
-                  </div>
+                  <TimelineItem key={index}>
+                    <TimelinePoint />
+                    <TimelineContent className="w-11/12">
+                      <TimelineTitle className="flex flex-col">
+                        <Link href={`/Post/${x._id}`}>{x.title}</Link>
+                      </TimelineTitle>
+                    </TimelineContent>
+                  </TimelineItem>
                 );
               })}
+            </Timeline>
+          </div>
+          <div className="w-4/12">
+            <Timeline>
               {review.result.map((x: any, index: number) => {
                 return (
-                  <div key={index}>
-                    {x.files.length > 0 ? (
-                      <Link href={`/Post/${x._id}`} className="">
-                        <img
-                          src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/uploadedFiles/${x.files}`}
-                          alt={x.title}
-                        />
-                      </Link>
-                    ) : (
-                      <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-56 sm:h-64 xl:h-80 2xl:h-96 place-content-center">
-                        <p className="text-white text-4xl w-8/12 mx-auto text-center">
-                          {x.title}
-                        </p>
-                      </div>
-                    )}
-                  </div>
+                  <TimelineItem key={index}>
+                    <TimelinePoint />
+                    <TimelineContent className="w-11/12">
+                      <TimelineTitle className="flex flex-col">
+                        <Link href={`/Post/${x._id}`}>{x.title}</Link>
+                      </TimelineTitle>
+                    </TimelineContent>
+                  </TimelineItem>
                 );
               })}
+            </Timeline>
+          </div>
+          <div className="w-4/12">
+            <Timeline>
               {tips.result.map((x: any, index: number) => {
                 return (
-                  <div key={index}>
-                    {x.files.length > 0 ? (
-                      <Link href={`/Post/${x._id}`} className="">
-                        <img
-                          src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/uploadedFiles/${x.files}`}
-                          alt={x.title}
-                        />
-                      </Link>
-                    ) : (
-                      <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-56 sm:h-64 xl:h-80 2xl:h-96 place-content-center">
-                        <p className="text-white text-4xl w-8/12 mx-auto text-center">
-                          {x.title}
-                        </p>
-                      </div>
-                    )}
-                  </div>
+                  <TimelineItem key={index}>
+                    <TimelinePoint />
+                    <TimelineContent className="w-11/12">
+                      <TimelineTitle className="flex flex-col">
+                        <Link href={`/Post/${x._id}`}>{x.title}</Link>
+                      </TimelineTitle>
+                    </TimelineContent>
+                  </TimelineItem>
                 );
               })}
-            </Carousel>
-          </div>
-          <br />
-          <div className="flex w-full">
-            <div className="w-4/12">
-              <Timeline>
-                {news.result.map((x: any, index: number) => {
-                  return (
-                    <TimelineItem key={index}>
-                      <TimelinePoint />
-                      <TimelineContent className="w-11/12">
-                        <TimelineTitle className="flex flex-col">
-                          <Link href={`/Post/${x._id}`}>{x.title}</Link>
-                        </TimelineTitle>
-                      </TimelineContent>
-                    </TimelineItem>
-                  );
-                })}
-              </Timeline>
-            </div>
-            <div className="w-4/12">
-              <Timeline>
-                {review.result.map((x: any, index: number) => {
-                  return (
-                    <TimelineItem key={index}>
-                      <TimelinePoint />
-                      <TimelineContent className="w-11/12">
-                        <TimelineTitle className="flex flex-col">
-                          <Link href={`/Post/${x._id}`}>{x.title}</Link>
-                        </TimelineTitle>
-                      </TimelineContent>
-                    </TimelineItem>
-                  );
-                })}
-              </Timeline>
-            </div>
-            <div className="w-4/12">
-              <Timeline>
-                {tips.result.map((x: any, index: number) => {
-                  return (
-                    <TimelineItem key={index}>
-                      <TimelinePoint />
-                      <TimelineContent className="w-11/12">
-                        <TimelineTitle className="flex flex-col">
-                          <Link href={`/Post/${x._id}`}>{x.title}</Link>
-                        </TimelineTitle>
-                      </TimelineContent>
-                    </TimelineItem>
-                  );
-                })}
-              </Timeline>
-            </div>
+            </Timeline>
           </div>
         </div>
-
-        <TabsHome />
       </div>
-    </Suspense>
+
+      <TabsHome />
+    </div>
   );
 };
 
